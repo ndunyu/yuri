e60d6eb567e5ea76e17384def94da41dc6cb33dc
Keith wachira <keithwachira@gmail.com>
2019-11-01 14:26:42 +0300

    added models

---
+++ models.go
@@ -0,0 +1,21 @@
+/**
+This will hold all models data  will
+
+be shared across all the
+structs
+
+
+*/
+
+package yuri
+
+import "time"
+
+type Models struct {
+	ID int `json:"id"`
+
+	CreatedAt time.Time `json:"created_at"`
+
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `pg:",soft_delete"`
+}
