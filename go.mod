c71e40af46e1aa6516e405a9c2f71d38106d4562
Keith wachira <keithwachira@gmail.com>
2019-11-04 15:53:37 +0300

    added url params

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ndunyu/yuri
 
 go 1.13
 
-require github.com/go-pg/pg/v9 v9.0.0 // indirect
+require (
+	github.com/go-chi/chi v4.0.2+incompatible // indirect
+	github.com/go-pg/pg/v9 v9.0.0 // indirect
+)
--- util.go
+++ util.go
@@ -2,9 +2,12 @@ package yuri
 
 import (
 	"math/rand"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/go-chi/chi"
 )
 
 func MakePin(min, max, n int) string {
@@ -24,4 +27,20 @@ func random(min, max int) int {
 	rand.Seed(time.Now().UnixNano())
 
 	return rand.Intn(max-min) + min
+}
+
+func makeHashId() {
+
+}
+
+func ExtractIntUrlParams(field string, r *http.Request) int {
+	i, _ := strconv.Atoi(chi.URLParam(r, field))
+	return i
+
+}
+
+func ExtractStringUrlParams(field string, r *http.Request) string {
+	i := chi.URLParam(r, field)
+	return i
+
 }
