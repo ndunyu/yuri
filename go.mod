489fcb2b98f282a1f6b05c8692c9021ef06efa1d
Keith wachira <keithwachira@gmail.com>
2019-11-11 12:12:30 +0300

    added token validation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ndunyu/yuri
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-chi/chi v4.0.2+incompatible // indirect
 	github.com/go-pg/pg/v9 v9.0.0 // indirect
 )
--- util.go
+++ util.go
@@ -1,12 +1,16 @@
 package yuri
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -22,6 +26,78 @@ func MakePin(min, max, n int) string {
 	return strings.Join(pin, "")
 
 }
+
+
+func CheckTokenId(r *http.Request, JwtSecretKey []byte) (int, error) {
+
+	authHeader := r.Header.Get("Authorization")
+	log.Println("below")
+	if authHeader == "" {
+
+		return 0, errors.New("authorized")
+
+	}
+	splitToken := strings.Split(authHeader, "Bearer")
+	authHeader = splitToken[1]
+	sentToken := strings.TrimSpace(authHeader)
+	log.Println(sentToken)
+
+	if sentToken == "" {
+
+		return 0, errors.New("authorized")
+
+	}
+
+	userId, err := ValidateToken(sentToken,JwtSecretKey)
+	if err != nil {
+		return 0, errors.New("unauthorized")
+
+	}
+	return userId, nil
+
+}
+
+func ValidateToken(tok string, JwtSecretKey []byte) (int, error) {
+	var userId int
+
+	token, err := jwt.Parse(tok, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+		return JwtSecretKey, nil
+
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userId := int(claims["user_id"].(float64)) //.(uint)
+		return userId, err
+
+	} else {
+		err = new("Wrong User Credentials sent")
+
+		return userId, err
+
+	}
+
+}
+
+type WrongTokenCredential struct {
+	s string
+}
+
+func (e *WrongTokenCredential) Error() string {
+	return e.s
+}
+
+func new(text string) error {
+	return &WrongTokenCredential{text}
+}
+
+
+
+
+
+
 
 func random(min, max int) int {
 	//rand.Seed(time.Now().Unix())
