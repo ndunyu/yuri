60dcdf2ebca35c036b59126b5f39a785bfb5a63f
Keith wachira <keithwachira@gmail.com>
2020-04-08 10:51:17 +0300

    adde status code 401

--- errors.go
+++ errors.go
@@ -5,6 +5,7 @@ import "net/http"
 var ErrNotFound = &ErrResponse{HTTPStatusCode: 404, StatusText: "Item not found."}
 var ErrInvalidRequest = &ErrResponse{HTTPStatusCode: http.StatusBadRequest, StatusText: "Invalid request."}
 var ErrInternalServerError = &ErrResponse{HTTPStatusCode: http.StatusInternalServerError, StatusText: "Internal Server Error"}
+var ErrUnauthorized = &ErrResponse{HTTPStatusCode: http.StatusUnauthorized, StatusText: "Authorization Error"}
 
 type ErrResponse struct {
 	HTTPStatusCode int    `json:"http_status_code"`
