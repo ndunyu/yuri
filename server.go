1a346430502ba023f06b3c21e118c1be96be5c71
Keith wachira <keithwachira@gmail.com>
2020-04-23 17:22:11 +0300

    remove faber

--- server.go
+++ server.go
@@ -1,30 +1,3 @@
 package yuri
 
 
-/*
-func main(){
-
-	app := fiber.New()
-
-	app.Static("/", "./public")
-
-	app.Listen(8080)
-}*/
-///package main
-
-/*import "github.com/gofiber/fiber"
-
-func main() {
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Body()
-
-		c.Send("Hello, World!")
-		c.Next()
-		c.JSON()
-		c.BodyParser()
-	})
-
-	app.Listen(3000)
-}*/
