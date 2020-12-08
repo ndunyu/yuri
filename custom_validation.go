8c132d4661f7438bdb5b5dc70c8cedbef68fa5c6
Keith wachira <keithwachira@gmail.com>
2020-12-08 09:06:55 +0300

    added africa talking sms

--- custom_validation.go
+++ custom_validation.go
@@ -11,9 +11,7 @@ func PhoneNumberValidation(fl validator.FieldLevel) bool {
 	return true
 }
 
-
-
-func TranslateErrors(trans ut.Translator,err error){
+func TranslateErrors(trans ut.Translator, err error) {
 	errs := err.(validator.ValidationErrors)
 	for _, e := range errs {
 		// can translate each error one at a time.
