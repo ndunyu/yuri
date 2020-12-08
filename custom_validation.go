dd6463839fddf9a571e3a6ce1b414a03e8285aed
Keith wachira <keithwachira@gmail.com>
2020-12-08 10:28:12 +0300

    changed to v3

--- custom_validation.go
+++ custom_validation.go
@@ -11,22 +11,23 @@ func PhoneNumberValidation(fl validator.FieldLevel) bool {
 	return true
 }
 
-func TranslateErrors(trans ut.Translator, err error) []string {
-	var t []string
+func TranslateErrors(trans ut.Translator, err error) map[int]string {
+
 	errs := err.(validator.ValidationErrors)
 	fmt.Println(errs.Translate(trans))
-	for _, e := range errs {
+	em := make(map[int]string)
+	for m, e := range errs {
 		// can translate each error one at a time.
+         em[m]=e.Translate(trans)
+		///fmt.Println(e.Namespace())
+		//fmt.Println(e.StructField())
+		//fmt.Println(e.Field())
+		//fmt.Println(e.Tag())
+		///t = append(t, e.Translate(trans))
 
-		fmt.Println(e.Namespace())
-		fmt.Println(e.StructField())
-		fmt.Println(e.Field())
-		fmt.Println(e.Tag())
-		t = append(t, e.Translate(trans))
-
-		fmt.Println(e.Translate(trans))
+		//fmt.Println(e.Translate(trans))
 	}
 
-	return t
+	return em
 
 }
