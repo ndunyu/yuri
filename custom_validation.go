8925f36b1853e070555873d8a0f3e49e002e2ca4
Keith wachira <keithwachira@gmail.com>
2020-12-08 08:38:46 +0300

    added translation errors

--- custom_validation.go
+++ custom_validation.go
@@ -1,8 +1,22 @@
 package yuri
 
-import "github.com/go-playground/validator/v10"
+import (
+	"fmt"
+
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+)
 
 func PhoneNumberValidation(fl validator.FieldLevel) bool {
-
 	return true
+}
+
+
+func TranslateErrors(trans ut.Translator,err error){
+	errs := err.(validator.ValidationErrors)
+	for _, e := range errs {
+		// can translate each error one at a time.
+		fmt.Println(e.Translate(trans))
+	}
+
 }
