535c7cf0e56f30070fcb0d5c3967d99552e2e64a
Keith Wachira <keithwachira@gmail.com>
2021-01-15 12:36:24 +0300

    added c2b

--- mpesa_models.go
+++ mpesa_models.go
@@ -110,7 +110,6 @@ type C2BValidationAndConfirmationResponse struct {
 	MiddleName        string
 	OrgAccountBalance string
 	ThirdPartyTransID string
-	///This should be string
 	TransAmount       float64
 	TransID           string
 	TransTime         string
