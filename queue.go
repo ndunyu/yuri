a508445e13c46f2c81ab5d48d73f27298c0bb4ae
Keith Wachira <keithwachira@gmail.com>
2021-04-26 10:19:26 +0300

    clean up

--- queue.go
+++ queue.go
@@ -7,14 +7,10 @@ import (
 
 ///Will queue jobs to be executed later
 
-///send to be enqueued
-type Processv1 func(interface{})
+// Process send to be enqueued
 type Process interface {
 	Process(interface{})
 }
-
-
-
 type Queue struct {
 	///Number of workers(consumers)goroutines to open
 	Workers int
@@ -26,39 +22,43 @@ type Queue struct {
 	///timeout in seconds
 	///TimeOut  time.Duration
 	//QuitChan will be used to close this worker
+	//terminate the goroutines jobs
 	QuitChan chan struct{}
-	///when a job is received what should happen ?(call back)
-	JobCallBack  Process
+	//JobCallBack is the function to be called when a job is received
+	//it should implement Process
+	//when a job is received what should happen ?(call back)
+	JobCallBack Process
 }
 
-func NewWorker(workers int,capacity int,jobCallBack Process) Queue {
+// NewWorker create a new job queue
+func NewWorker(workers int, capacity int, jobCallBack Process) Queue {
 	var wg sync.WaitGroup
-	jobQueue := make(chan interface{},capacity )
-	//cancelChan := make(chan bool)
+	jobQueue := make(chan interface{}, capacity)
 	quit := make(chan struct{})
 	return Queue{
-		Workers:  workers,
-		JobQueue: jobQueue,
+		Workers:     workers,
+		JobQueue:    jobQueue,
 		JobCallBack: jobCallBack,
-		Wg:       &wg,
-		QuitChan: quit,
+		Wg:          &wg,
+		QuitChan:    quit,
 	}
 
 }
 
-//close or the running goroutines
+// Stop close or the running goroutines
+// and stops processing any more jobs
 func (q *Queue) Stop() {
-	log.Println("called here")
+
 
 	close(q.QuitChan)
 	//if &q.QuitChan!=nil {
 	//	close(q.QuitChan)
 	//}
 
-
 }
 
-
+// EnqueueJob use this to queue the jobs you need to execute
+//Note you set the buffer size with capacity
 //Returns false if the buffer is full
 ///else if it is accepted it returns true
 func (q *Queue) EnqueueJob(job interface{}) bool {
@@ -70,17 +70,13 @@ func (q *Queue) EnqueueJob(job interface{}) bool {
 	}
 }
 
-///start the workers add add them to wait group
+// StartWorkers start the workers add add them to wait group
 func (q *Queue) StartWorkers() {
 	for i := 0; i < q.Workers; i++ {
-
 		q.Wg.Add(1)
 		go q.worker()
-		///go q.worker(jobChan)
 	}
-	log.Println("am waiting")
-	///q.Wg.Wait()
-	log.Println("finished")
+
 }
 
 //each goroutine runs this
@@ -88,9 +84,9 @@ func (q *Queue) worker() {
 	defer q.Wg.Done()
 	for {
 		select {
-		case  <-q.QuitChan:
+		case <-q.QuitChan:
 
-			log.Println("its closing me")
+			log.Println("closing the  workers")
 			return
 
 		case job := <-q.JobQueue:
@@ -99,6 +95,3 @@ func (q *Queue) worker() {
 		}
 	}
 }
-///TryEnqueue(job Job, jobChan <-chan Job)
-
-///TryEnqueue(job Job, jobChan chan<- Job)
