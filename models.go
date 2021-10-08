66fc4f4e0cd3e05566d138cb5620f2b1a26efcf9
Keith wachira <keithwachira@gmail.com>
2021-10-08 12:26:50 +0300

    upgraded packags

--- models.go
+++ models.go
@@ -14,8 +14,8 @@ import "time"
 type Models struct {
 	ID int `json:"id"`
 
-	CreatedAt time.Time `json:"created_at" sql:"DEFAULT:current_timestamp"`
 
+	CreatedAt time.Time `json:"created_at" sql:"DEFAULT:current_timestamp"`
 	UpdatedAt time.Time `json:"updated_at" sql:"DEFAULT:current_timestamp"`
 	DeletedAt time.Time `pg:",soft_delete"`
 }
